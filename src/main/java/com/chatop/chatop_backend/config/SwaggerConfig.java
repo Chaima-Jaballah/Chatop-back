edaead20056d9084a96d4ea937ef659c461ce1e0
Chaima Jaballah <shaymadsi@gmail.com>
2025-03-28 05:33:54 +0100

    Update Swagger configuration to add JWT authorization button

--- src/main/java/com/chatop/chatop_backend/config/SwaggerConfig.java
+++ src/main/java/com/chatop/chatop_backend/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.chatop.chatop_backend.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,11 +10,20 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 	@Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Chatop API")
                         .version("1.0")
-                        .description("Documentation de l'API de location Chatop"));
+                        .description("Documentation de l'API de location Chatop"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 
 }
